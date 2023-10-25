a8f1cdd43a1dbc63347aaf1efc626c2b4202daf6
Isaac Iniongun <iniongunisaac@gmail.com>
2023-10-25 17:34:14 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DojahWidget",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DojahWidget",
+            targets: ["DojahWidget"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DojahWidget"),
+        .testTarget(
+            name: "DojahWidgetTests",
+            dependencies: ["DojahWidget"]),
+    ]
+)
