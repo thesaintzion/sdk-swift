1c2653514ae8361e32cf5ae4a0b8ecc0d7ce1967
shittu33 <shittuabdulmujeeb@gmail.com>
2025-05-26 06:53:11 +0100

    Refactor (cleanup govData prefill)

--- Sources/DojahWidget/Features/GovernmentData/GovernmentDataViewController.swift
+++ Sources/DojahWidget/Features/GovernmentData/GovernmentDataViewController.swift
@@ -59,38 +59,7 @@ final class GovernmentDataViewController: DJBaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        let manualSelectGovData = preference.DJExtraUserData?.govData
-        
-        if(manualSelectGovData != nil && manualSelectGovData!.isFilled()){
-            let items = viewModel.governmentIDs.names
-            let symbols = self.viewModel.governmentIDs.symbols
-            
-            if(manualSelectGovData?.bvn?.isNotEmpty == true){
-                let index = symbols.firstIndex(of: "bvn") ?? 0
-                self.govtIDView.selectItem(item: items[index],index:index)
-                self.govtIDNumberTextField.text = manualSelectGovData?.bvn ?? ""
-            }else  if(manualSelectGovData?.dl?.isNotEmpty == true){
-                let index = symbols.firstIndex(of: "dl") ?? 0
-                self.govtIDView.selectItem(item: items[index],index:index)
-                self.govtIDNumberTextField.text = manualSelectGovData?.dl ?? ""
-            }else  if(manualSelectGovData?.nin?.isNotEmpty == true){
-                let index = symbols.firstIndex(of: "nin") ?? 0
-                self.govtIDView.selectItem(item: items[index],index:index)
-                self.govtIDNumberTextField.text = manualSelectGovData?.nin ?? ""
-            }else  if(manualSelectGovData?.vnin?.isNotEmpty == true){
-                let index = symbols.firstIndex(of: "vnin") ?? 0
-                self.govtIDView.selectItem(item: items[index],index:index)
-                self.govtIDNumberTextField.text = manualSelectGovData?.vnin ?? ""
-            }
-            
-            self.govtIDView.isUserInteractionEnabled = false
-            self.govtIDNumberTextField.isUserInteractionEnabled = false
-            govtIDNumberTextField.showView(true)
-        }else{
-            self.govtIDView.isUserInteractionEnabled = true
-            self.govtIDNumberTextField.isUserInteractionEnabled = true
-            govtIDNumberTextField.showView(false)
-        }
+        prefillDataIfAvailable()
     }
     
     private func setupUI() {
@@ -130,6 +99,42 @@ final class GovernmentDataViewController: DJBaseViewController {
         }
         
         navView.showSuccessMessage("Fill the form as it appears on your valid ID")
+    }
+    
+    private func prefillDataIfAvailable() {
+        
+        let manualSelectGovData = preference.DJExtraUserData?.govData
+        
+        if(manualSelectGovData != nil && manualSelectGovData!.isFilled()){
+            let items = viewModel.governmentIDs.names
+            let symbols = self.viewModel.governmentIDs.symbols
+            
+            if(manualSelectGovData?.bvn?.isNotEmpty == true){
+                let index = symbols.firstIndex(of: "bvn") ?? 0
+                self.govtIDView.selectItem(item: items[index],index:index)
+                self.govtIDNumberTextField.text = manualSelectGovData?.bvn ?? ""
+            }else  if(manualSelectGovData?.dl?.isNotEmpty == true){
+                let index = symbols.firstIndex(of: "dl") ?? 0
+                self.govtIDView.selectItem(item: items[index],index:index)
+                self.govtIDNumberTextField.text = manualSelectGovData?.dl ?? ""
+            }else  if(manualSelectGovData?.nin?.isNotEmpty == true){
+                let index = symbols.firstIndex(of: "nin") ?? 0
+                self.govtIDView.selectItem(item: items[index],index:index)
+                self.govtIDNumberTextField.text = manualSelectGovData?.nin ?? ""
+            }else  if(manualSelectGovData?.vnin?.isNotEmpty == true){
+                let index = symbols.firstIndex(of: "vnin") ?? 0
+                self.govtIDView.selectItem(item: items[index],index:index)
+                self.govtIDNumberTextField.text = manualSelectGovData?.vnin ?? ""
+            }
+            
+            self.govtIDView.isUserInteractionEnabled = false
+            self.govtIDNumberTextField.isUserInteractionEnabled = false
+            govtIDNumberTextField.showView(true)
+        }else{
+            self.govtIDView.isUserInteractionEnabled = true
+            self.govtIDNumberTextField.isUserInteractionEnabled = true
+            govtIDNumberTextField.showView(false)
+        }
     }
     
 }
